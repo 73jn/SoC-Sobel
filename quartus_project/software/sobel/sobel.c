b03516980ed3c0413d9a71e1c1b759e380a4bbd0
73jn <jean.nanchen@gmail.com>
2021-12-18 18:49:56 +0100

    Unrolling first loop modifications

--- quartus_project/software/sobel/sobel.c
+++ quartus_project/software/sobel/sobel.c
@@ -55,11 +55,9 @@ short sobel_mac( unsigned char *pixels,
    short dy,dx;
    short result = 0;
    for (dy = -1 ; dy < 2 ; dy++) {
-      for (dx = -1 ; dx < 2 ; dx++) {
          result += filter[(dy+1)*3+(0)]*pixels[(y+dy)*width+(x+(-1))];
          result += filter[(dy+1)*3+(1)]*pixels[(y+dy)*width+(x)];
          result += filter[(dy+1)*3+(2)]*pixels[(y+dy)*width+(x+1)];
-      }
    }
    return result;
 }
